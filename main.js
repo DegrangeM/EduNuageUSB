bb5f910710b3e01dae49c2721a0f9a7a3f700e31
DegrangeM <53106394+DegrangeM@users.noreply.github.com>
2022-10-16 23:44:30 +0200

    Auto create rclone config after logging

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, dialog } = require('electron')
 const path = require('path')
+const child = require('child_process').execFile;
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
@@ -15,8 +16,66 @@ function createWindow () {
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
 
+  const mainWindow2 = new BrowserWindow({
+    width: 800,
+    height: 600,
+    autoHideMenuBar: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  mainWindow2.loadURL('https://nuage.apps.education.fr')
+  mainWindow2.webContents.on('dom-ready', function () {
+    const currentURL = new URL(this.getURL())
+    if (/^nuage[0-9]+\.apps\.education\.fr$/.test(currentURL.hostname)) {
+      mainWindow2.webContents.executeJavaScript(`fetch(new URL(location.href).origin + '/index.php/settings/personal/authtokens', {
+        method: 'POST',
+        body: '{"name":"EduNuageUSB"}',
+        headers:{
+          'requesttoken':window.oc_requesttoken,
+          'Content-Type': 'application/json'
+        }
+      }).then(
+        r => r.json()
+      )`, true).catch(
+        e=>{
+          dialog.showErrorBox("Erreur lors de la création de la clef d'application", e)
+          app.quit()
+        }
+      ).then(
+        r=>{
+          loginName = r.loginName;
+          token = r.token;
+          child(".\\rclone\\rclone.exe",
+          [
+            'config',
+            'create',
+            'EduNuageUSB',
+            'webdav',
+            'vendor=nextcloud',
+            'url="https://nuage03.apps.education.fr/remote.php/dav/files/' + loginName + '/"',
+            'user="' + loginName + '"',
+            'pass="' + token + '"',
+            '--obscure',
+            '--config',
+            './.rclone.conf',
+          ], function (err, data) {
+            console.log(err)
+            console.log(data.toString());
+          });
+        }
+      );
+    }
+  })
+
+
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
+
+  child(".\\rclone\\rclone.exe", ["version"], function (err, data) {
+    console.log(err)
+    console.log(data.toString());
+  });
 }
 
 // This method will be called when Electron has finished
@@ -24,20 +83,8 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
-
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+  app.quit()
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
