464b6784c30b97a7eb81e09650eabd82647d41f8
DegrangeM <53106394+DegrangeM@users.noreply.github.com>
2022-12-03 17:31:31 +0100

    Update login.js

--- src/main/lib/login.js
+++ src/main/lib/login.js
@@ -18,8 +18,8 @@ function login(EduNuageUSB) {
 
         EduNuageUSB.loginWindow.webContents.on('dom-ready', function () {
             const currentURL = new URL(this.getURL())
-            if(EduNuageUSB.log) {
-                fs.appendFile('log.txt', currentURL + '\n', () => {});
+            if (EduNuageUSB.log) {
+                fs.appendFile('log.txt', currentURL + '\n', () => { });
             }
             if (/^nuage[0-9]+\.apps\.education\.fr$/.test(currentURL.hostname)) {
                 if (currentURL.pathname === '/index.php/logout') {
@@ -81,17 +81,36 @@ function login(EduNuageUSB) {
                     );
                 } // endif not logout
             } // endif nuage url
+            /*
+                Problématique : l'enseignant qui se connecte pour la première fois n'a pas de compte LaBoîte
+                Lors de son premier accès, il lui sera demandé de se créer un compte LaBoite
+                Cela le sort du processus classique de EduNuageUSB, il faut donc gérer ça.
+                Le gros problème est qu'une fois le compte LaBoîte créé, l'enseignant n'est pas redirigé vers
+                nuage. Il faut donc le rediriger vers nuage une fois le compte LaBoite créé. Il n'est pas
+                nécessaire que l'enseignant choisisse sa structure.
+            */
             else if (currentURL.href === 'https://nuage.apps.education.fr/') {
                 // Il est possible que l'utilisateur n'ait pas de compte LaBoite
                 // Il va lui être demandé d'en créer un mais cela ouvre une nouvelle fenêtre non traçable
-                // On remplace l'ouverture d'une nouvelle fenêtre par une rediraction
+                // On remplace l'ouverture d'une nouvelle fenêtre par une redirection
                 EduNuageUSB.loginWindow.webContents.executeJavaScript(`
                     window.open2 = window.open;
                     window.open = function(url) {
+                        if(url==="https://portail.apps.education.fr/signin") {
+                            url = "https://portail.apps.education.fr/"
+                            // On redirige vers cette page pour éviter de déclancher la condition suivante
+                            // Le site redirigera localement vers /signin mais sans déclancher la condition suivante
+                        }
                         location.replace(url);
                     };0
                 `);
                 // On aurait aussi pu utiliser https://www.electronjs.org/docs/latest/api/web-contents#contentssetwindowopenhandlerhandler
+            }
+            else if (currentURL.href === 'https://portail.apps.education.fr/signin') {
+                // De manière contre intuitive, il s'agit de l'url une fois connecté à la boite
+                // Le site redirigeant ensuite localement vers https://portail.apps.education.fr/
+                EduNuageUSB.loginWindow.webContents.executeJavaScript('location.href = https://nuage.apps.education.fr;', true);
+                // On redirige vers nuages.apps.education.fr qui redirigera lui vers le bon nextcloud (nuage02, nuage03, etc.)
             }
         }); // dom-ready
     }
